d1c3ac4999ece1d4ce49f1950e94341a2511ed15
VOL1TZ1 <91470979+VOL1TZ1@users.noreply.github.com>
2025-11-01 14:33:02 +0200

    Added Login Conditional Routing

---
+++ app/complex-dashboard/@login/page.tsx
@@ -0,0 +1,9 @@
+import Card from "@/app/components/card";
+
+export default function LoginSlot() {
+    return (
+      <Card>
+        <h1>Please Login to Continue</h1>
+      </Card>
+    );
+  } 
