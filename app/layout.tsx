0acbc449266d78574b441abe80cc3c714627f7a9
VOL1TZ1 <91470979+VOL1TZ1@users.noreply.github.com>
2025-11-01 12:57:54 +0200

    Add ErrorWrapper and GlobalError components for error handling

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ErrorWrapper } from "./error-wrapper";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,8 +34,9 @@ export default function RootLayout({
         <header style={{backgroundColor:"lightblue",padding:"1rem"}}>
         <p>Header</p>
       </header>
-      
+      <ErrorWrapper>
         {children}
+      </ErrorWrapper>
 
         <footer style={{backgroundColor:"ghostwhite",padding:"1rem"}}>
         <p>Footer</p>
